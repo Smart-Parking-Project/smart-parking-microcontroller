d49cbb4408dcff2f0509490521087229440fbbd7
danielTura <dderese1@gmail.com>
2021-03-08 13:59:35 -0500

    update_print_statement

--- sensor_and_led_orchesterater.py
+++ sensor_and_led_orchesterater.py
@@ -23,17 +23,22 @@ a.pinMode(ledPin1, a.OUTPUT)
 print("-----> The sensor started <-----")
 try:
     a.digitalWrite(ledPin1, a.LOW)
+    current_state = False
     while True:
         sleep(0.5)
         distance = ultrasonic1.get_distance()
         if distance >= 0 and distance < 30:
             on = threading.Thread(
-                target=updateParkingSpaceStatus, args=("6035c931a1ffe80204b8de3a", True)
+                target=updateParkingSpaceStatus, args=(
+                    "6035c931a1ffe80204b8de3a", True)
             )
             on.start()
-            print(f"{distance}cm \n")
+            if current_state == False:
+                print(f"{distance}cm \n")
+                current_state = True
             a.digitalWrite(ledPin1, a.HIGH)
         else:
+            current_state = False
             a.digitalWrite(ledPin1, a.LOW)
             off = threading.Thread(
                 target=updateParkingSpaceStatus,
